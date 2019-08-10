822eeb79577c3517cfbc446dcc47680e39d21ef5
Aaron Beckman <aaronpbeckman@gmail.com>
2019-08-10 00:33:35 -0500

    Easy data entry complete

--- rocket.py
+++ rocket.py
@@ -14,8 +14,9 @@ class Rocket:
         houston.info('rocket.Rocket.__init__ initializing webdriver')
 
         try:
-            site = webdriver.Chrome(executable_path=self.chromedriver_path)
-            site.get(self.destination_url)
+            self.site = webdriver.Chrome(
+                executable_path=self.chromedriver_path)
+            self.site.get(self.destination_url)
         except Exception as e:
             houston.error('rocket.Rocket.__init__ {}'.format(e))
         # TODO: literally everything
@@ -23,12 +24,44 @@ class Rocket:
     def prepare_boosters(self, permit_num, birth_date):
         houston.info('rocket.Rocket.prepare_boosters entering in credentials')
 
+        time.sleep(0.5)
+        road_test_link = self.site.find_element_by_link_text(
+            "Schedule or Reschedule an Exam")
+        road_test_link.click()
+
+        time.sleep(0.5)
+        permit_entry = self.site.find_element_by_id("c-8")
+        permit_entry.send_keys(permit_num)
+
+        time.sleep(0.5)
+        dob_entry = self.site.find_element_by_id("c-9")
+        dob_entry.send_keys(birth_date)
+
+        time.sleep(0.5)
+        next_button = self.site.find_element_by_id("c-__NextStep")
+        next_button.click()
+
+        time.sleep(0.5)
+        next_button = self.site.find_element_by_id("caption2_c-42")
+        next_button.click()
+
     def scan_systems(self, zip_code):
         houston.info(
             'rocket.Rocket.scan_systems scanning available testing locations')
-        # TODO: literally everything
-
         self.previous_scan = time.time()
+        
+        time.sleep(0.5)
+        zip_entry = self.site.find_element_by_id("c-02")
+        zip_entry.send_keys(zip_code)
+
+        time.sleep(0.5)
+        zip_button = self.site.find_element_by_link_text("Show locations close to Zip")
+        zip_button.click()
+        time.sleep(1)
+
+        # TODO: loop through available locations near ZIP, call takeoff() if necessary
+
+        # TODO: loop through all locations, call takeoff() if necessary
 
     def takeoff(self):
         houston.info('rocket.Rocket.takeoff notifying user')
